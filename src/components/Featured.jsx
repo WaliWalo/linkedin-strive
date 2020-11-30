c4ab841e645792df1e81d20d12f0bb5bc2d62794
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-30 17:17:05 +0400

    Update Featured.jsx

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 
 export default class Featured extends Component {
   render() {
-    return 
-      (   <div>
-
-
-
-          </div>;)
+    return (
+      <div>
+        <section className="featured">
+          <h2 className="featured-text">Featured</h2>
+          <div className="featured-description">
+            <div className="featured-description-text">
+              <strong>Showcase your work</strong>
+              <span>
+                by featuring your best posts, documents, media, and websites.
+              </span>
+            </div>
+            <div className="featured-description-close"></div>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
