8799cc2eb26fe794d87666e05848af54f9eb528f
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-30 20:17:03 +0400

    Update Featured.jsx

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Featured.css";
 
 export default class Featured extends Component {
   render() {
     return (
-      <div>
-        <section className="featured">
-          <h2 className="featured-text">Featured</h2>
-          <div className="featured-description">
-            <div className="featured-description-text">
-              <strong>Showcase your work </strong>
-              <span>
-                by featuring your best posts, documents, media, and websites.
-              </span>
+      <Container>
+        <Row>
+          <Col md={10} className="featured">
+            <h2 className="featured-text">Featured</h2>
+            <div className="featured-description">
+              <div className="featured-description-text">
+                <p>
+                  <strong>Showcase your work </strong>
+                  <span>
+                    by featuring your best posts, documents, media, and
+                    websites.
+                  </span>
+                </p>
+
+                <p className="small-text">Add Featured</p>
+              </div>
+              <div className="featured-description-close">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  fill="currentColor"
+                  class="mercado-match"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path d="M14 3.41L9.41 8 14 12.59 12.59 14 8 9.41 3.41 14 2 12.59 6.59 8 2 3.41 3.41 2 8 6.59 12.59 2z"></path>
+                </svg>
+              </div>
             </div>
-            <div className="featured-description-close"></div>
-          </div>
-        </section>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
