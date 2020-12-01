a4309328bf6539b68670558153b2d0813ccad7e9
rakkas95 <rakkas95@users.noreply.github.com>
2020-12-01 09:25:07 +0000

    viewedPeople Added with fix required

--- src/components/PeopleViewed.jsx
+++ src/components/PeopleViewed.jsx
@@ -1,38 +1,53 @@
 import React, { Component } from "react";
 import { fetchListOfProfiles } from '../api/linkedinApi'
 import Profiles from "./Profiles";
+import {  Card, ListGroup, } from 'react-bootstrap'
 
 
 class PeopleViewed extends Component {
-  
+
   state = {
     profiles: []
   };
 
+
   async componentDidMount() {
     let profiles = await fetchListOfProfiles();
-    console.log(profiles);
-    this.setState({  profiles: profiles });
+    this.setState({ profiles: profiles });
   }
 
- 
-  
+
+
   render() {
     
     return (
       <div>
         {this.state.profiles && (
-         <div> {this.state.profiles.map((profile, index) => (
-          <Profiles key={index} profiles={profile} />
-        ))} </div>
+          <div className='d-flex justify-content-end'>
+            
+            <Card className='mr-5' style={{ width: '18rem' }}>
+              <h4 className='mt-4'>People also Viewed</h4>
+              <Card.Body>
+                
+                  
+                    <ListGroup className="list-group-flush">
+                    {this.state.profiles.map((profile, index) => (
+                      <Profiles key={index} profiles={profile} />
+                    ))}
+                </ListGroup> 
+                
+                
+                  </Card.Body>
+              
+            </Card>
+          </div>
         )}
-        
 
-        PeopleViewed
-        
+
+
       </div>
-    
-    ) 
+
+    )
   }
 }
 
--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap'
+import './Profiles.css'
+import { Row, Col, Card, ListGroupItem, Button } from 'react-bootstrap'
+
 
 class Profiles extends React.Component {
     state = {
-        profile : this.props.profiles,
+        profile: {},
     }
-    render() {
-        const { profile } = this.state
-        return (
-            <div>
-                <Card style={{ width: '18rem' }}>
-                    
-                    <Card.Body>
-                        <Card.Img variant="top" src={profile.image} style={{width: '80px', height: '80px'}} />
-                        <Card.Title>{profile.name} &nbsp; {profile.surname }</Card.Title>
-                        <Card.Text>
-                            {profile.title}
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
 
-            </div>
+    render() {
+    let viewedPeople = [];
+    for (let i = 0; i < 5; i++) {
+        viewedPeople.push(this.state.profile[Math.floor(Math.random() *this.state.profile.length) + 1])
+        console.log(viewedPeople[i])
+    };
+        return (
+            <>
+                {
+                    viewedPeople.slice(0, 6).map(profile =>
+                        <ListGroupItem>
+                    <Row>
+                        <Col xs={2} className="pl-0 pr-1 my-1">
+                        <Card.Img className="profile-img" src={this.state.profile.image} stye={{width: "50px", height: "50px", borderRadius: "50%"}} />
+                        </Col>
+                        <Col xs={8} className="pl-1 pr-0">
+                            <Card.Title className="title">{this.state.profile.name} {this.state.profile.surname}</Card.Title>
+                                <Card.Text>
+                                    {this.state.profile.title}
+                            </Card.Text>
+                            
+                        </Col>
+                        <Col xs={2}>
+                        <Button border="secondary" style={{borderRadius: "70%"}}><i class="fab fa-telegram-plane"></i>hello </Button>
+                        </Col>
+                    </Row>
+                                
+                            </ListGroupItem>
+                    )}
+                
+                        
+            </>
         )
     }
 }
