8da07cf7de3b4a59828cf6bbfb60e844d5946407
Hung Jin Chong <hungjinchong@Outlook.com>
2020-12-01 09:49:30 +0000

    fixed randomness

--- src/components/PeopleViewed.jsx
+++ src/components/PeopleViewed.jsx
@@ -1,54 +1,51 @@
 import React, { Component } from "react";
-import { fetchListOfProfiles } from '../api/linkedinApi'
+import { fetchListOfProfiles } from "../api/linkedinApi";
 import Profiles from "./Profiles";
-import {  Card, ListGroup, } from 'react-bootstrap'
-
+import { Card, ListGroup } from "react-bootstrap";
 
 class PeopleViewed extends Component {
-
   state = {
-    profiles: []
+    profiles: [],
+    randomProfiles: [],
   };
-
 
   async componentDidMount() {
     let profiles = await fetchListOfProfiles();
-    this.setState({ profiles: profiles });
+    let viewedPeople = [];
+    if (profiles.length !== 0) {
+      for (let i = 0; i < 5; i++) {
+        viewedPeople.push(
+          profiles[Math.floor(Math.random() * profiles.length) + 1]
+        );
+      }
+    }
+    this.setState({ profiles: profiles, randomProfiles: viewedPeople });
   }
 
-
-
   render() {
-    
     return (
       <div>
-        {this.state.profiles && (
-          <div className='d-flex justify-content-end'>
-            
-            <Card className='mr-5' style={{ width: '18rem' }}>
-              <h4 className='mt-4'>People also Viewed</h4>
+        {this.state.profiles && this.state.randomProfiles ? (
+          <div className="d-flex justify-content-end">
+            <Card className="mr-5" style={{ width: "18rem" }}>
+              <h4 className="mt-4">People also Viewed</h4>
               <Card.Body>
-                
-                  
-                    <ListGroup className="list-group-flush">
-                    {this.state.profiles.map((profile, index) => (
+                <ListGroup className="list-group-flush">
+                  {this.state.randomProfiles
+                    .slice(0, 6)
+                    .map((profile, index) => (
                       <Profiles key={index} profiles={profile} />
                     ))}
-                </ListGroup> 
-                
-                
-                  </Card.Body>
-              
+                </ListGroup>
+              </Card.Body>
             </Card>
           </div>
+        ) : (
+          "SOMETHING WENT WRONG"
         )}
-
-
-
       </div>
-
-    )
+    );
   }
 }
 
-export default PeopleViewed
+export default PeopleViewed;
--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,47 +1,40 @@
-import React from 'react'
-import './Profiles.css'
-import { Row, Col, Card, ListGroupItem, Button } from 'react-bootstrap'
-
+import React from "react";
+import "./Profiles.css";
+import { Row, Col, Card, ListGroupItem, Button } from "react-bootstrap";
 
 class Profiles extends React.Component {
-    state = {
-        profile: {},
-    }
+  state = {
+    profile: this.props.profiles,
+  };
 
-    render() {
-    let viewedPeople = [];
-    for (let i = 0; i < 5; i++) {
-        viewedPeople.push(this.state.profile[Math.floor(Math.random() *this.state.profile.length) + 1])
-        console.log(viewedPeople[i])
-    };
-        return (
-            <>
-                {
-                    viewedPeople.slice(0, 6).map(profile =>
-                        <ListGroupItem>
-                    <Row>
-                        <Col xs={2} className="pl-0 pr-1 my-1">
-                        <Card.Img className="profile-img" src={this.state.profile.image} stye={{width: "50px", height: "50px", borderRadius: "50%"}} />
-                        </Col>
-                        <Col xs={8} className="pl-1 pr-0">
-                            <Card.Title className="title">{this.state.profile.name} {this.state.profile.surname}</Card.Title>
-                                <Card.Text>
-                                    {this.state.profile.title}
-                            </Card.Text>
-                            
-                        </Col>
-                        <Col xs={2}>
-                        <Button border="secondary" style={{borderRadius: "70%"}}><i class="fab fa-telegram-plane"></i>hello </Button>
-                        </Col>
-                    </Row>
-                                
-                            </ListGroupItem>
-                    )}
-                
-                        
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <ListGroupItem>
+          <Row>
+            <Col xs={2} className="pl-0 pr-1 my-1">
+              <Card.Img
+                className="profile-img"
+                src={this.state.profile.image}
+                stye={{ width: "50px", height: "50px", borderRadius: "50%" }}
+              />
+            </Col>
+            <Col xs={8} className="pl-1 pr-0">
+              <Card.Title className="title">
+                {this.state.profile.name} {this.state.profile.surname}
+              </Card.Title>
+              <Card.Text>{this.state.profile.title}</Card.Text>
+            </Col>
+            <Col xs={2}>
+              <Button border="secondary" style={{ borderRadius: "70%" }}>
+                <i className="fab fa-telegram-plane"></i>hello{" "}
+              </Button>
+            </Col>
+          </Row>
+        </ListGroupItem>
+      </>
+    );
+  }
 }
 
-export default Profiles
+export default Profiles;
