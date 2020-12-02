e3a934ad3623b4f617f2bc5e60b8cc86b973aa55
rakkas95 <rakkas95@users.noreply.github.com>
2020-12-02 16:18:20 +0000

    sidebarright draft

--- src/components/SidebarRight.jsx
+++ src/components/SidebarRight.jsx
@@ -1,7 +1,104 @@
 import React, { Component } from "react";
+import './SideBarRight.css'
+import { Card, ListGroup } from 'react-bootstrap'
+import  pic from '../images/dream-job.jpg'
+
 
 export default class SidebarRight extends Component {
   render() {
-    return <div>SIDERIGHT</div>;
+    return <div>
+      <Card header className="mb-2">
+        LinkedIn News
+        <ListGroup variant="flush">
+          <ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item>
+    <ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item>
+  </ListGroup> 
+      </Card>
+      
+
+      {/* trending courses */}
+      <Card header className="mb-2">
+        Today's trending courses
+        <ListGroup variant="flush">
+          <ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item>
+    <ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item>
+  </ListGroup> 
+      </Card>
+      
+        {/* image */}
+        <Card header className="mb-2">
+        <img src={pic} alt="" style={{ width: "100%"}}/>
+      </Card>
+      
+        {/* add to your feed */}
+        <Card header className="mb-2">
+        LinkedIn News
+        <ListGroup variant="flush">
+          <ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item>
+    <ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item><ListGroup.Item className='no-border'>
+            <a href="http://"> OECD issues warning for UK economy</a>
+            <br / >
+             <small className='text-muted'> 10h ago • 120 readers </small>
+            </ListGroup.Item>
+  </ListGroup> 
+        </Card>
+    </div>;
+
   }
 }
